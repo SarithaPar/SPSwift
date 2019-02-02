c6ca5f2984405988838dd14a682bd4d84225830f
Saritha Parsoya <sparsoya@ebay.com>
2019-02-01 22:27:27 -0800

    Basic commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# SPSwift
---
+++ TutorialPractice/BasicOne.playground/Contents.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+var label = "Width is "
+var width = 100
+var width2 = 1
+
+let widthLabel = label + String(width) + " pixel"
+let widthLabel1 = "Width is \(width) pixel"
+let widthLabel2 = "Width2 is  \(width + width2) pixel"
+
+var list = ["a", "b", "c"]
+list[1] = "xyz"
+list.append("pqr")
+print(list)
+
+var dict = [
+    "Aman" : "Verma",
+    "Anshul" : "Nindra",
+    "Dhruvi" : "Parsoya"
+]
+print(dict)
+
+let emptyArray = ()
+print(emptyArray)
+
+let emptyDictionary = [String: Float]()
+print(emptyDictionary)
+
+
+
+
+
+
