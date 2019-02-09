ea72887c8735fbc820c4a13e66e39ed7fe343d39
Saritha Parsoya <sparsoya@ebay.com>
2019-02-09 11:03:12 -0800

    Created workspace for tutorial

---
+++ SPSwiftProject/SPSwiftProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SPSwiftProject
+//
+//  Created by Parsoya, Saritha(AWF) on 2/9/19.
+//  Copyright © 2019 Parsoya, Saritha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
